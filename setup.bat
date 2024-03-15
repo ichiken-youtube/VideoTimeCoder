f6f2f447a2930ff699fdefc8f00ec9443eb6595e
ICHIKEN <inquiry@ichiken-engineering.com>
2024-03-15 10:25:32 +0900

    設定ファイル追跡停止、setupで生成できるようにした

---
+++ setup.bat
@@ -0,0 +1,17 @@
+@echo off
+
+echo Installing ffmpeg...
+winget install ffmpeg
+
+echo Installing Libraries...
+pip install ffmpeg-python
+
+echo Generating setting file...
+if not exist settings.py (
+    echo FILESIZE_LIMIT = 95 > settings.py
+    echo SIZE = ^(1280, 720^) >> settings.py
+    echo FPS = 29.97 >> settings.py
+) else (
+    echo settings.py already exists.
+)
+pause
