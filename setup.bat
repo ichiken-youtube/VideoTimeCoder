91bffb24d9e8eebae316874d636147f81973a7c8
CarbCraftBrewer <inquiry@ichiken-engineering.com>
2024-05-23 12:40:44 +0900

    セットアップ時の初期書き込み設定項目からtextを削除

--- setup.bat
+++ setup.bat
@@ -11,7 +11,6 @@ if not exist settings.py (
     echo FILESIZE_LIMIT = 95 > settings.py
     echo FRAME_SIZE = ^(1280, 720^) >> settings.py
     echo FPS = 29.97 >> settings.py
-    echo TEXT = ^"YOUR EYES ONLY!! DO NOT POST!!^" >> settings.py
 ) else (
     echo settings.py already exists.
 )
