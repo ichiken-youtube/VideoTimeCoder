55eff065177303d702cf04a6656f21c9c0bfbf50
CarbCraftBrewer <inquiry@ichiken-engineering.com>
2024-05-23 12:07:19 +0900

    セットアップスクリプトで追加設定項目の自動生成に対応。変数名の可読性向上。

--- setup.bat
+++ setup.bat
@@ -9,9 +9,11 @@ pip install ffmpeg-python
 echo Generating setting file...
 if not exist settings.py (
     echo FILESIZE_LIMIT = 95 > settings.py
-    echo SIZE = ^(1280, 720^) >> settings.py
+    echo FRAME_SIZE = ^(1280, 720^) >> settings.py
     echo FPS = 29.97 >> settings.py
+    echo TIMECODE_SIZE = 135  >> settings.py
     echo TEXT = ^"YOUR EYES ONLY!! DO NOT POST!!^" >> settings.py
+    echo TEXT_SIZE = 70 >> settings.py
 ) else (
     echo settings.py already exists.
 )
