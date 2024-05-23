aabe3e89b8979ab1bbe30af6e54e4c0dacc3dbeb
CarbCraftBrewer <inquiry@ichiken-engineering.com>
2024-05-23 12:32:26 +0900

    settings.pyのデフォルト書き込み項目削除

--- setup.bat
+++ setup.bat
@@ -11,9 +11,7 @@ if not exist settings.py (
     echo FILESIZE_LIMIT = 95 > settings.py
     echo FRAME_SIZE = ^(1280, 720^) >> settings.py
     echo FPS = 29.97 >> settings.py
-    echo TIMECODE_SIZE = 135  >> settings.py
     echo TEXT = ^"YOUR EYES ONLY!! DO NOT POST!!^" >> settings.py
-    echo TEXT_SIZE = 70 >> settings.py
 ) else (
     echo settings.py already exists.
 )
